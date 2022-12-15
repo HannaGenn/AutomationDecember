6a778acb96bf5dfa1e3a92e4bc8dd2d7e1034867
Hanna Peniaz <anya.penyaz@mail.ru>
2022-12-15 12:04:25 +0300

    add methods in class + other changes

--- AutomationDecember/Program.cs
+++ AutomationDecember/Program.cs
@@ -1,80 +1,50 @@
 ﻿using System;
+using AutomationDecember;
+
 
 namespace allExercises
 {
     internal class Program
     {
-        static void DeleteSymbol(string enteredText)
-        {
-            Console.WriteLine("Enter symbol to delete from the text:");
-            string symbol = Console.ReadLine();
-            Console.Write("Text without deleted symbol \"{0}\" is:", symbol);
-            char[] textToDisplayInChar = enteredText.ToCharArray();
-            for (int i = 0; i < textToDisplayInChar.Length; i++)
-            {
-                string elementFromMassiveToCompare = char.ToString(textToDisplayInChar[i]);
-                bool firstComparison = (elementFromMassiveToCompare == symbol);
-                bool secondComparison = (elementFromMassiveToCompare.ToUpper() == symbol.ToUpper());
-
-                if (!firstComparison && !secondComparison)
-                {
-                    Console.Write(textToDisplayInChar[i]);
-                }
-            }
-
-        }
-
-        static void CharacterToCharacter(string enteredText)
-        {
-            char[] textLineInChar = enteredText.ToCharArray();
-            for (int i = 0; i < textLineInChar.Length; i++)
-            {
-                Console.WriteLine(textLineInChar[i]);
-            }
-        }
-
-        static void Reverse(string enteredText)
-        {
-            char[] writeTextInChar = enteredText.ToCharArray();
-            char[] reversedTextInChar = new char[writeTextInChar.Length];
-            int reversedArraySymbolIndex = 0;
-            for (int i = writeTextInChar.Length - 1; i >= 0; i--)
-            {
-                reversedTextInChar[reversedArraySymbolIndex] = writeTextInChar[i];
-                Console.Write(reversedTextInChar[reversedArraySymbolIndex]);
-            }
-        }
-
-
         static void Main()
         {
-            while (true)
+            do
             {
-                Console.WriteLine("Select exercise to be done with text: Delete symbol from the text(1),character-by-character text(2), reverse text(3)");
+                TextEditor.PrintText("Select exercise to be done with text: \nDelete symbol from the text(1),\nDelete symbol from text in RegularExpressions(2),\nCharacter-by-character text(3),\nReverse case(4)");
                 int variant = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter your text:");
+                TextEditor.PrintText("Enter your text:");
                 string textToWrite = Console.ReadLine();
-                while (true)
+                if (variant == 1)
                 {
-                    if (variant == 1)
-                    {
-                        DeleteSymbol(textToWrite);
-                    }
-                    else if (variant == 2)
-                    {
-                        CharacterToCharacter(textToWrite);
-                    }
-                    else if (variant == 3)
-                    {
-                        Reverse(textToWrite);
-                    }
-                    Console.Write("\n");
-                    Console.Write("Press Enter to continue doing exercises!");
-                    Console.ReadLine();
-                    break;
+                    TextEditor.PrintText("Enter symbol to delete from the text:");
+                    string symbol = Console.ReadLine();
+                    Console.Write("Text without deleted symbol \"{0}\" is:", symbol);
+                    var newText = TextEditor.DeleteSymbol(textToWrite, symbol);
+                    TextEditor.PrintText(newText);
                 }
+                else if (variant == 2)
+                {
+                    TextEditor.PrintText("Enter symbol to delete from the text using Regular expressions:");
+                    string symbol = Console.ReadLine();
+                    Console.Write("Text without deleted symbol \"{0}\" is:", symbol);
+                    var newText = TextEditor.DeleteByRegularExpressions(textToWrite, symbol);
+                    TextEditor.PrintText(newText);
+                }
+                else if (variant == 3)
+                {
+                    TextEditor.CharacterToCharacter(textToWrite);
+                }
+                else if (variant == 4)
+                {
+                    var newText = TextEditor.ReverseCase(textToWrite);
+                    TextEditor.PrintText(newText);
+                }
+                TextEditor.PrintText("\nPress Enter to continue doing exercises or press the Escape (Esc) to quit!");
             }
-
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
         }
     }
 }
+
+
+
---
+++ AutomationDecember/TextEditor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace AutomationDecember
+{
+    public class TextEditor
+    {
+        public static void PrintText(string newText)
+        {
+            Console.WriteLine(newText);
+        }
+        public static string DeleteSymbol(string enteredText, string symbol)
+        {
+            char[] textToDisplayInChar = enteredText.ToCharArray();
+            char[] result = new char[textToDisplayInChar.Length];
+            var j = 0;
+            var charSymbol = char.Parse(symbol);
+            for (int i = 0; i < textToDisplayInChar.Length; i++)
+            {
+                if ((textToDisplayInChar[i] != charSymbol) && (char.ToUpper(textToDisplayInChar[i]) != char.ToUpper(charSymbol)))
+                {
+                    result[j] = textToDisplayInChar[i];
+                    j++;
+                }
+            }
+            return new string(result);
+        }
+        public static string DeleteByRegularExpressions(string enteredText, string symbol)
+        {
+            string spaceElement = "";
+            Regex regexCase = new Regex($"[{symbol}]", RegexOptions.IgnoreCase);
+            string result = regexCase.Replace(enteredText, spaceElement);
+            return result;
+        }
+        public static void CharacterToCharacter(string enteredText)
+        {
+            char[] textLineInChar = enteredText.ToCharArray();
+            for (int i = 0; i < textLineInChar.Length; i++)
+            {
+                Console.WriteLine(textLineInChar[i]);
+            }
+        }
+        public static string ReverseCase(string enteredText)
+        {
+            char[] writeTextInChar = enteredText.ToCharArray();
+            char[] reversedTextInChar = new char[writeTextInChar.Length];
+            for (int i = 0; i < writeTextInChar.Length; i++)
+            {
+                if (writeTextInChar[i] == char.ToLower(writeTextInChar[i]))
+                {
+                    reversedTextInChar[i] = char.ToUpper(writeTextInChar[i]);
+                }
+                else if (writeTextInChar[i] == char.ToUpper(writeTextInChar[i]))
+                {
+                    reversedTextInChar[i] = char.ToLower(writeTextInChar[i]);
+                }
+
+            }
+            return new string(reversedTextInChar);
+
+        }
+
+        /*        public static string ReverseCase(string enteredText)
+                {
+                    char[] writeTextInChar = enteredText.ToCharArray();
+                    char[] reversedTextInChar = new char[writeTextInChar.Length];
+                    int indexOfFirstElement = Array.IndexOf(writeTextInChar, writeTextInChar[0]);
+
+                    for (int i = 0; i < writeTextInChar.Length; i++)
+                    {
+                        char elementFromInitialMassive = writeTextInChar[indexOfFirstElement];
+                        if (elementFromInitialMassive == char.ToLower(elementFromInitialMassive))
+                        {
+                            reversedTextInChar[indexOfFirstElement] = char.ToUpper(writeTextInChar[indexOfFirstElement]);
+                            indexOfFirstElement++;
+                        }
+                        else if (elementFromInitialMassive == char.ToUpper(elementFromInitialMassive))
+                        {
+                            reversedTextInChar[indexOfFirstElement] = char.ToLower(writeTextInChar[indexOfFirstElement]);
+                            indexOfFirstElement++;
+                        }
+                    }
+                    return new string(reversedTextInChar);
+
+                }*/
+    }
+
+}
